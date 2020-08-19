da7f1521de7b50aa90a787a3dc01943670227ade
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-08-19 19:57:40 +0800

    Prepare environment for widget

--- src/app/maps-widget/maps-widget.component.ts
+++ src/app/maps-widget/maps-widget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-maps-widget',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maps-widget.component.css']
 })
 export class MapsWidgetComponent implements OnInit {
+  env = environment.GOOGLE_MAPS_API_KEY;
 
   constructor() { }
 
